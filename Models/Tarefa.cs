eba8521ab238298b3be3e9f1c583ca8db031b68b
Kimberly Santos <kimberly.santos@volvo.com>
2025-11-28 23:17:15 -0300

    Permisison for Angular

--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Models;
 
-[Table ("Tarefas")]
+[Table("Tarefas")]
 public class Tarefa
 {
     [Key]
--- Program.cs
+++ Program.cs
@@ -3,23 +3,29 @@ using PACEserver.Contexts;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
-        options.JsonSerializerOptions.ReferenceHandler = 
+        options.JsonSerializerOptions.ReferenceHandler =
             System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles);
 
 builder.Services.AddControllers()
     .AddJsonOptions(options => options.JsonSerializerOptions
         .DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull);
 
-builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 string sqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(sqlConnection));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular",
+        b => b.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+});
 
 var app = builder.Build();
 
@@ -31,6 +37,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAngular");
+
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
